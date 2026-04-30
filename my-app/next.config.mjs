4d251873e21e1a05e48eeae81ae58f6462ab697d
Evan Asti <easti@ucsd.edu>
2026-04-30 12:05:39 -0700

    3.1 complete

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/my-app/' : '',
+  basePath: isProd ? '/my-app' : '',
+  output: 'export'
 };
 
 export default nextConfig;
