c1ae57c47a748acb895bb8d02ceb1950f6d96862
Evan Asti <easti@ucsd.edu>
2026-06-11 05:10:22 -0700

    New config

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,14 +1,9 @@
-/** @type {import('next').NextConfig} */
-
 const repo = "/nextjs-cogs125";
 
-const nextConfig = {
+export default {
   output: "export",
   basePath: repo,
-  images: {
-    unoptimized: true,
-  },
+  assetPrefix: repo,
+  images: { unoptimized: true },
   trailingSlash: true,
 };
-
-export default nextConfig;
