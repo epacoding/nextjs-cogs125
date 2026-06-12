f7b2824b34adaaab8822155c78756e7bb90508f0
Evan Asti <easti@ucsd.edu>
2026-06-12 03:39:43 -0700

    deploy

---
+++ _next/static/fszmagI7l4tQjIYFjNk5m/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nextjs-cogs125//_next/:path+",
+        "destination": "/nextjs-cogs125/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
